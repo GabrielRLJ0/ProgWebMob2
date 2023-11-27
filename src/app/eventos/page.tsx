3472693fcbe04ea5b32b38e3c81ea82708041709
GabrielRLJ0 <gabrielrlaraj@gmail.com>
2023-11-27 20:22:00 -0300

    funfando Deus é bom

--- src/app/eventos/page.tsx
+++ src/app/eventos/page.tsx
@@ -87,8 +87,8 @@ export default function Eventos() {
 
   return (
     <div className={`
-     flex justify-center items-center h-screen
-     bg-gradient-to-bl from-indigo-900 via-indigo-400 to-indigo-900
+     flex justify-center items-center min-h-screen
+     bg-gradient-to-bl from-indigo-1900 via-indigo-400 to-indigo-3900
      text-white`}>
       <Layout titulo="Cadastro de eventos">
         {visivel === 'tabela' ? (
